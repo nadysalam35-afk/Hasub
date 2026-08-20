4844e8811135b794425bed97fc126c9d7104ef55
yousef4000 <yousefnady38@gmail.com>
2026-08-21 02:21:05 +0300

    Update project files

---
+++ script.js
@@ -0,0 +1,47 @@
+alert ("Hello, World!")
+var myVariable = 10;
+var myString = "This is a string.";
+var myBoolean = true;
+var dicimalNumber = 3.14;
+
+const myConstant = "This value cannot be changed.";
+const pr = 3.14;
+document.write("Welcome to my page!");
+let head=document.getElementById("head");
+head.innerHTML = "This is yousef nady page";
+X
+let number1 = 5;
+let number2 = 10;
+let sum = number2 ** number1;
+console.log("the result is:" ** sum);
+
+number1++;
+console.log(number1);
+number2--;
+console.log(number2);
+
+let x = 5,
+
+    y = 10;
+
+let result = x < y;
+console.log(result);
+
+let result = 5 < 7 && 9 > 6;
+
+console.log(result);
+let result = 5 < 7 || 9 > 6;
+console.log(result);
+
+let acadymy = "hasoub";
+console.log(acadymy.length);
+console.log(acadymy.toUpperCase());
+console.log(acadymy.toLowerCase());
+console.log(acadymy.indexOf("h"));
+console.log(acadymy.slice(0, 3));
+console.log(acadymy.replace("hasoub", "yousef"));
+console.log(acadymy.charAt(2));
+console.log(acadymy.substr(0, 3));
+
+
+
